93811712259950b9fd9e36518bcdcaef5ad3c607
justinkook <justinkook@gmail.com>
2022-09-30 23:43:48 -0500

    Add status id element to show average price

--- src/App.js
+++ src/App.js
@@ -15,11 +15,9 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
           }
 
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Average Price</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p id="status" className="App-intro"></p>
       </div>
     );
   }
